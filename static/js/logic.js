a9e391e402a5611abb6306fde3d1779ef05bb369
EHVargas22 <elsa.h.vargas@gmail.com>
2022-01-17 15:23:35 -0500

    add map layers

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,8 +10,29 @@ var defaultMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
 // make map object
 var myMap = L.map("map", {
     center: [36.7783, -119.4179],
-    zoom: 3
+    zoom: 3,
+    layers: [defaultMap, grayscale]
 });
 
-// ad default map
-defaultMap.addto(myMap);
+// add grayscale layer
+var grayscale = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+});
+
+// make basemaps object
+let basemaps = {
+    Default: defaultMap,
+    GrayScale: grayscale
+};
+
+// add default map
+defaultMap.addTo(myMap);
+
+// add layer control
+L.control
+    .layers(basemaps)
+    .addTo(myMap);
